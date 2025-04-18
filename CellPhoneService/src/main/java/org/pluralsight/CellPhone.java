edb9270d82a31f06b1ed5a923627eaefa2b393e8
Hayat A <xaetbek@gmail.com>
2025-04-18 11:32:14 -0700

    CellPhoneService project is done.
    Completed the Step 2.
    Created a new instance of a CellPhone, then prompt the user for the cell phone information.
    Used the setters of your CellPhone object to add the values entered by the user. Using the getters of the CellPhone print the properties of the phone to the screen.

--- CellPhoneService/src/main/java/org/pluralsight/CellPhone.java
+++ CellPhoneService/src/main/java/org/pluralsight/CellPhone.java
@@ -1,10 +1,53 @@
 package org.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhone {
+    // Initialize variables
     private long serialNumber;
     private String model;
     private String carrier;
-    private int phoneNumber;
+    private String phoneNumber;
+    private String owner;
+
+    //Global scanner object "read"
+    Scanner read = new Scanner(System.in);
+
+    // Build a parameterless constructor
+    public CellPhone () {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+
+    // Prompt user for input and Use setter
+    public void setUp(){
+        System.out.print("\nWhat is the serial number? - ");
+        setSerialNumber(read.nextLong());
+        read.nextLine();
+
+        System.out.print("What model is the phone? - " );
+        setModel(read.nextLine());
+
+        System.out.print("Who is the carrier? - ");
+        setCarrier(read.nextLine());
+
+        System.out.print("What is the phone number? - ");
+        setPhoneNumber(read.nextLine());
+
+        System.out.print("Who is the owner of the phone? - ");
+        setOwner(read.nextLine());
+    }
+
+    // Display the user's input
+    public void displayInfo(){
+        System.out.println("\nHello, " + getOwner());
+        System.out.println("This is your new " + getModel() + " with serial number " + getSerialNumber());
+        System.out.println("Your carrier is " + getCarrier() + " and your phone number is " + getPhoneNumber());
+        System.out.println("Enjoy your new phone!");
+    }
 
     public long getSerialNumber() {
         return serialNumber;
@@ -30,11 +73,11 @@ public class CellPhone {
         this.carrier = carrier;
     }
 
-    public int getPhoneNumber() {
+    public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(int phoneNumber) {
+    public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
@@ -44,15 +87,5 @@ public class CellPhone {
 
     public void setOwner(String owner) {
         this.owner = owner;
-    }
-
-    private String owner;
-
-    public class CellPhone() {
-        this.serialNumber = 0;
-        this.model = "";
-        this.carrier = "";
-        this.phoneNumber = "";
-        this.owner = "";
     }
 }
--- CellPhoneService/src/main/java/org/pluralsight/CellPhoneApplication.java
+++ CellPhoneService/src/main/java/org/pluralsight/CellPhoneApplication.java
@@ -2,6 +2,11 @@ package org.pluralsight;
 
 public class CellPhoneApplication {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        CellPhone cellphone1 = new CellPhone();
+
+        cellphone1.setUp();
+        cellphone1.displayInfo();
+
     }
 }
