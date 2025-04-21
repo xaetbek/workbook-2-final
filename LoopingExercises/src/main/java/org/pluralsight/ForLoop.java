2e120f4655bd92967e4879d1e999b35c403a9eb9
Hayat A <xaetbek@gmail.com>
2025-04-20 19:40:19 -0700

    Added for loop

--- LoopingExercises/src/main/java/org/pluralsight/ForLoop.java
+++ LoopingExercises/src/main/java/org/pluralsight/ForLoop.java
@@ -2,6 +2,9 @@ package org.pluralsight;
 
 public class ForLoop {
     public void main() {
-        
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Lunch");
     }
 }
--- LoopingExercises/src/main/java/org/pluralsight/Main.java
+++ LoopingExercises/src/main/java/org/pluralsight/Main.java
@@ -5,10 +5,13 @@ public class Main {
 
         WhileLoop whileLoop = new WhileLoop();
         DoWhileLoop doWhileLoop = new DoWhileLoop();
+        ForLoop forLoop = new ForLoop();
 
+        System.out.print("\n-----||--While Loop--||-----\n");
         whileLoop.main();
-        System.out.println("\n-----||||-----\n");
+        System.out.print("\n-----||--Do While Loop--||-----\n");
         doWhileLoop.main();
-
+        System.out.print("\n-----||--For Loop--||-----\n");
+        forLoop.main();
     }
 }
